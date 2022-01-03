dce1b4db024156c86b4c5553531fac53151e0151
gon-2 <dydrndi1@naver.com>
2022-01-03 15:15:38 +0900

    vo 업데이트

--- alone/src/main/java/com/kh/alone/vo/ClassInfoVo.java
+++ alone/src/main/java/com/kh/alone/vo/ClassInfoVo.java
@@ -2,25 +2,19 @@ package com.kh.alone.vo;
 
 public class ClassInfoVo {
 	String c_group;  // 주간반, 야간반, 주말반 
-	String c_cate;   // 수업종류(프로그램인지 요리인지 기타등등분류)
 	String c_title;  // 수업제목
 	String c_period; // 수업기간
 	int classroom;   // 교실(몇호실인지)
 	int person;      // 인원수
 	public ClassInfoVo() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
 	public String getC_group() {
 		return c_group;
 	}
 	public void setC_group(String c_group) {
 		this.c_group = c_group;
-	}
-	public String getC_cate() {
-		return c_cate;
-	}
-	public void setC_cate(String c_cate) {
-		this.c_cate = c_cate;
 	}
 	public String getC_title() {
 		return c_title;
@@ -48,7 +42,8 @@ public class ClassInfoVo {
 	}
 	@Override
 	public String toString() {
-		return "ClassInfoVo [c_group=" + c_group + ", c_cate=" + c_cate + ", c_title=" + c_title + ", c_period="
-				+ c_period + ", classroom=" + classroom + ", person=" + person + "]";
+		return "ClassInfoVo [c_group=" + c_group + ", c_title=" + c_title + ", c_period=" + c_period + ", classroom="
+				+ classroom + ", person=" + person + "]";
 	}
+
 }
