d421f809594fb1930a8aaa9c9ab67bd98bac19e0
leejeong0495 <leejeong0495@naver.com>
2022-01-06 09:30:56 +0900

    [UPDATE] SQL 수정

--- alone/src/main/webapp/WEB-INF/SQL/HoonSql.sql
+++ alone/src/main/webapp/WEB-INF/SQL/HoonSql.sql
@@ -9,3 +9,18 @@ create table TBL_CLASS_BOARD(
     class_board_viewcnt number,
     class_board_comment varchar2(500)
 );
+
+drop table tbl_class_board;
+
+
+-- 고객센터 => 수강신청 테이블 ( 수정 후 )
+create table tbl_class_board(
+	class_board_number number primary key,
+	class_board_enroll varchar2(40) not null,
+	class_board_title varchar2(300) not null,
+	class_board_content varchar2(3000) not null,
+	class_board_userid varchar2(50) references tbl_member(userid) not null,
+    class_board_postdate timestamp default SYSDATE ,
+    class_board_viewcnt number,
+    class_board_comment varchar2(500)
+);
