f41e3247f156e67fd2e8b68a30f2fcc45d43b28d
leejeong0495 <leejeong0495@naver.com>
2022-01-14 11:15:08 +0900

    [update] 주석 업데이트

---
+++ alone/src/main/java/com/kh/alone/service/ServiceMessageService.java
@@ -0,0 +1,39 @@
+/*
+ * 2021-01-14
+ * Author : 이정훈
+ * code Explanation : 학생 상담 메시지 Service
+ */
+
+package com.kh.alone.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kh.alone.dao.ServiceMessageDao;
+import com.kh.alone.vo.ServiceMessageVo;
+
+@Service
+public class ServiceMessageService {
+	@Inject
+	private ServiceMessageDao messagedao;
+	
+	// 메세지 보내기
+	@Transactional
+	public void sendMessage(ServiceMessageVo messagevo , String service_message_receiver) {
+		messagedao.sendMessage(messagevo);
+		messagedao.receiveCount(service_message_receiver);
+	}
+	
+	// 받은 사람의 메시지
+	public List<ServiceMessageVo> recieveMessage(String service_message_receiver){
+		List<ServiceMessageVo> recieverList = messagedao.recieveMessage(service_message_receiver);
+		System.out.println("ServiceMessageService , receiveCount , recieverList >> " + recieverList);
+		return recieverList;
+	}
+
+
+}
---
+++ alone/src/main/java/com/kh/alone/vo/ServiceMessageVo.java
@@ -0,0 +1,93 @@
+/*
+ * 2021-01-14
+ * Author : 이정훈
+ * code Explanation : 학생 상담 메시지 Vo
+ */
+
+
+package com.kh.alone.vo;
+
+import java.sql.Timestamp;
+
+public class ServiceMessageVo {
+	private int service_message_number;
+	private String service_message_sender;
+	private String service_message_receiver;
+	private String service_message_title;
+	private String service_message_content;
+	private int service_message_count;
+	private Timestamp service_message_date;
+	
+	public ServiceMessageVo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getService_message_number() {
+		return service_message_number;
+	}
+
+	public void setService_message_number(int service_message_number) {
+		this.service_message_number = service_message_number;
+	}
+
+	public String getService_message_sender() {
+		return service_message_sender;
+	}
+
+	public void setService_message_sender(String service_message_sender) {
+		this.service_message_sender = service_message_sender;
+	}
+
+	public String getService_message_receiver() {
+		return service_message_receiver;
+	}
+
+	public void setService_message_receiver(String service_message_receiver) {
+		this.service_message_receiver = service_message_receiver;
+	}
+
+	public String getService_message_title() {
+		return service_message_title;
+	}
+
+	public void setService_message_title(String service_message_title) {
+		this.service_message_title = service_message_title;
+	}
+
+	public String getService_message_content() {
+		return service_message_content;
+	}
+
+	public void setService_message_content(String service_message_content) {
+		this.service_message_content = service_message_content;
+	}
+
+	public Timestamp getService_message_date() {
+		return service_message_date;
+	}
+
+	public void setService_message_date(Timestamp service_message_date) {
+		this.service_message_date = service_message_date;
+	}
+
+	public int getService_message_count() {
+		return service_message_count;
+	}
+
+	public void setService_message_count(int service_message_count) {
+		this.service_message_count = service_message_count;
+	}
+
+	@Override
+	public String toString() {
+		return "ServiceMessageVo [service_message_number=" + service_message_number + ", service_message_sender="
+				+ service_message_sender + ", service_message_receiver=" + service_message_receiver
+				+ ", service_message_title=" + service_message_title + ", service_message_content="
+				+ service_message_content + ", service_message_count=" + service_message_count
+				+ ", service_message_date=" + service_message_date + "]";
+	}
+	
+	
+
+}
